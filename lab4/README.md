7c4aea0b4eefb236370aa114e1356fc5f4330a1c
Seth <8thseth@gmail.com>
2022-05-10 12:27:16 -0300

    lab 4 corrigido

---
+++ lab4/README.md
@@ -0,0 +1,56 @@
+# Computação Concorrente | Laboratório 4
+
+## Tempo de Execução
+
+### D10<sup>5</sup>
+
+| Tipo | Tempo |
+| --- | --- |
+| **Sequencial** | 0,009940 |
+| **1 thread** | 0,008296 |
+| **2 threads** | 0,010536 |
+| **4 threads** | 0,010428 |
+
+### D10<sup>7</sup>
+
+| Tipo | Tempo |
+| --- | --- |
+| **Sequencial** | 5,733268 |
+| **1 thread** | 4,578899 |
+| **2 threads** | 2,487251 |
+| **4 threads** | 1,768267 |
+
+### D10<sup>8</sup>
+
+| Tipo | Tempo |
+| --- | --- |
+| **Sequencial** | 151,626807 |
+| **1 thread** | 124,052206 |
+| **2 threads** | 64,897324 |
+| **4 threads** | 35,694795 |
+
+## Aceleração
+
+### D10<sup>5</sup>
+
+| Threads | Aceleração |
+| --- | --- |
+| **1 thread** | 1,19 |
+| **2 threads** | 0,94 |
+| **4 threads** | 0,95 |
+
+### D10<sup>7</sup>
+
+| Threads | Aceleração |
+| --- | --- |
+| **1 thread** | 1,25 |
+| **2 threads** | 2,31 |
+| **4 threads** | 3,25 |
+
+### D10<sup>8</sup>
+
+| Threads | Aceleração |
+| --- | --- |
+| **1 thread** | 1,22 |
+| **2 threads** | 2,33 |
+| **4 threads** | 4,24 |
--- lab4/lab4.c
+++ lab4/lab4.c
@@ -4,13 +4,10 @@
 #include <math.h>
 #include "timer.h"
 
-long long int dim; // dimensao do vetor de entrada
-int nthreads; // numero de threads
-int global; // variavel compartilhada
-int *vetorEntrada; 
-float *vetorSeqSaida;
-float *vetorConSaida;
-pthread_mutex_t lock; //variavel de lock para exclusao mutua
+long long int dim, global = 0; // dimensao do vetor de entrada
+int nthreads, *vetor; // threads, vetor de entrada
+float *vetorSeq, *vetorCon; // vetores de saida
+pthread_mutex_t lock; // variavel de lock para exclusao mutua
 
 int ehPrimo(long long int n) {
   if (n<=1) return 0;
@@ -22,52 +19,39 @@ int ehPrimo(long long int n) {
 }
 
 // funcao sequencial
-void processaPrimosSeq(int **vetorEntrada, long long int dim) {
-  int primo = 1;
-  for(int i=0; i<dim; i++) {
-    int n = (*vetorEntrada)[i];
-    if (n<=1) primo = 0;
-    if (n==2) primo = 1;
-    if (n%2==0) primo = 0;
-    for (int i=3; i<sqrt(n)+1; i+=2)
-      if(n%i==0) primo = 0;
-    if (primo == 1)
-      vetorSeqSaida[i] = sqrt((*vetorEntrada)[i]);
+void processaPrimosSeq(int *vetor, long long int dim) {
+  for(long long int i=0; i<dim; i++) {
+    if (ehPrimo(vetor[i]))
+      vetorSeq[i] = sqrt(vetor[i]);
     else
-      vetorSeqSaida[i] = (*vetorEntrada)[i];
+      vetorSeq[i] = vetor[i];
   };
 }
 
-void* processaPrimosCon(void *arg) {
+void* processaPrimosCon() {
   pthread_mutex_lock(&lock);
   int i = global; global++;
   pthread_mutex_unlock(&lock);
 
   while(i < dim) {
-    if (ehPrimo(vetorEntrada[i]))
-      vetorConSaida[i] = sqrt(vetorEntrada[i]);
+    if (ehPrimo(vetor[i]))
+      vetorCon[i] = sqrt(vetor[i]);
     else
-      vetorConSaida[i] = vetorEntrada[i];
+      vetorCon[i] = vetor[i];
+
     pthread_mutex_lock(&lock);
     i = global; global++;
     pthread_mutex_unlock(&lock);
+
   }
-  // for(i; i<dim, i++) {
-  //   n = vetorEntrada[i]
-  //   if (ehPrimo(vetorEntrada[i])
-  //     vetorSaida[i] = sqrt(vetorEntrada[i]);
-  //   else
-  //     vetorSaida[i] = vetorEntrada[i];
-  // }
 
   pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[]) {
-  // pthread_t *tid;
-  pthread_t tid[NTHREADS];
-  int t, id[NTHREADS];
-  // double ini, fim;
+  pthread_t *tid;
+  long int t;
+  double ini, fim;
 
   // recebe e valida os parametros
   if(argc < 3) {
@@ -79,51 +63,58 @@ int main(int argc, char *argv[]) {
   nthreads = atoi(argv[2]);
 
   // alocacao de memoria dos vetores
-  vetorEntrada = (int*) malloc(sizeof(int)*dim);
-  if(vetorEntrada == NULL) { fprintf(stderr, "ERRO--malloc\n"); return 2; }
-  vetorSeqSaida = (float*) malloc(sizeof(float)*dim);
-  if(vetorSeqSaida == NULL) { fprintf(stderr, "ERRO--malloc\n"); return 2; }
-  vetorConSaida = (float*) malloc(sizeof(float)*dim);
-  if(vetorConSaida == NULL) { fprintf(stderr, "ERRO--malloc\n"); return 2; }
+  vetor = (int*) malloc(sizeof(int)*dim);
+  if(vetor == NULL) { fprintf(stderr, "ERRO--malloc\n"); return 2; }
+
+  vetorSeq = (float*) malloc(sizeof(float)*dim);
+  if(vetorSeq == NULL) { fprintf(stderr, "ERRO--malloc\n"); return 2; }
+  
+  vetorCon = (float*) malloc(sizeof(float)*dim);
+  if(vetorCon == NULL) { fprintf(stderr, "ERRO--malloc\n"); return 2; }
 
   // preenche o vetor de entrada com numeros aleatorios de 0 a 100
   srand(time(NULL));
 
   for(long int i = 0; i < dim; i++) {
-    vetorEntrada[i] = ((int) rand())%100;
+    vetor[i] = rand() % dim;
   };
 
   // faz o calculo sequencial
-  processaPrimosSeq(&vetorEntrada, dim);
+  GET_TIME(ini);
+  processaPrimosSeq(vetor, dim);
+  GET_TIME(fim);
+  printf("Tempo sequencial: %lf\n", fim - ini);
 
+  GET_TIME(ini);
   //inicilaiza o mutex (lock de exclusao mutua)
   pthread_mutex_init(&lock, NULL);
 
+  // cria as threads e aloca a estrutura
+  tid = (pthread_t*) malloc(sizeof(pthread_t)*nthreads);
+  if( tid == NULL ) { fprintf(stderr, "ERRO--malloc\n"); return 2;}
+
   // cria as threads
+
   for(t=0; t<nthreads; t++) {
-    id[t]=t;
-    if (pthread_create(&tid[t], NULL, processaPrimosCon, (void *) &id[t])) {
-      printf("--ERRO: pthread_create()\n"); exit(-1);
-    }
-  }
+    if (pthread_create(tid+t, NULL, processaPrimosCon, NULL)) {
+      printf("--ERRO: pthread_create()\n"); return 2;
+    };
+  };
 
   //espera todas as threads terminarem
-  for (t=0; t<NTHREADS; t++) {
-    if (pthread_join(tid[t], NULL)) {
-         printf("--ERRO: pthread_join() \n"); exit(-1); 
+  for (t=0; t<nthreads; t++) {
+    if (pthread_join(*(tid+t), NULL)) {
+         printf("--ERRO: pthread_join() \n"); return 2; 
     } 
   } 
-
+  
   pthread_mutex_destroy(&lock);
 
-  for(int i = 0; i < dim; i++) {
-    if(vetorConSaida[i] != vetorSeqSaida[i]) {
-      printf("diferente: %f \n", vetorConSaida[i]);
-    }    
-  };
+  GET_TIME(fim);
+  printf("Tempo concorrente: %lf\n", fim - ini);
   
-  free(vetorEntrada);
-  free(vetorSeqSaida);
+  free(vetor);
+  free(tid);
   
   return 0;
 }
