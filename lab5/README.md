e1b6be82a97ea8eb4c1ddddbd9ab80a0e5b27033
Seth <8thseth@gmail.com>
2022-05-18 21:10:43 -0300

    lab 5 atualizado

--- lab5/README.md
+++ lab5/README.md
@@ -3,17 +3,19 @@
 ## Atividade 1
 
 ### Questão 2.
+Sim. O objetivo do programa, que é "Bye" esperar para ser printado quando dois "Hello" já tiverem aparecido, foi confirmado em todas as tentativas.
 
 ### Questão 3.
+O programa nem chegou a encerrar, pois não vai ter mais ninguém para desbloquear a thread B que foi bloqueada, fazendo com que ela nunca conclua sua tarefa. 
+
+### Questão 5.
+Para o programa funcionar de maneira correta, alterei *pthread_cond_signal(&x_cond);* para *pthread_cond_broadcast(&x_cond);*, fazendo com que não seja apenas um sinal de desbloqueio mandado.
 
 ## Atividade 2
 
 ### Questão 2.
+Sim. As threads A só printaram "Bye" após B printar "Hello".
 
 ## Atividade 3
-
-### Questão 2.
-
-### Questão 3.
-
-### Questão 4.
+Com o uso de *if*, a condição só era checada antes do bloqueio. Isso quer dizer que, caso ela seja desbloqueada e o *x* se altere, é possível que essa condição não seja checada novamente e isso pode causar divergência no resultado.
+Quando alteramos para *while*, nós garantimos que o loop trate esse problema, fazendo com que a condição sempre volte a ser checada e atualizada, sendo mais recomendado o seu uso nessa situação. 
---
+++ lab5/lab5.c
@@ -0,0 +1,134 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <pthread.h>
+
+// uma thread por mensagem
+#define NTHREADS 4
+
+int total = 0; 
+pthread_mutex_t mutex; 
+pthread_cond_t cond_vinda, cond_volte; // variaveis de controle
+
+void *volteSempre (void *t) {
+  pthread_mutex_lock(&mutex);
+  while (total < 4) { // espera as outras threads
+    pthread_cond_wait(&cond_volte, &mutex);
+  }
+  pthread_mutex_unlock(&mutex); 
+
+  printf("Volte sempre!\n");
+  return NULL;
+}
+
+void *aVontade (void *t) {
+  pthread_mutex_lock(&mutex);
+  if (total == 0) { // espera a thread 5
+    pthread_cond_wait(&cond_vinda, &mutex);
+  }
+  pthread_mutex_unlock(&mutex); 
+
+  printf("Fique a vontade.\n");
+
+  pthread_mutex_lock(&mutex);
+  total++; // atualiza o total de mensagens
+  pthread_cond_signal(&cond_volte); // sinaliza caso a thread 1 esteja esperando
+  pthread_mutex_unlock(&mutex); 
+
+  return NULL;
+}
+
+void *senteSe (void *t) {
+  pthread_mutex_lock(&mutex);
+  if (total == 0) { // espera a thread 5
+    pthread_cond_wait(&cond_vinda, &mutex);
+  }
+  pthread_mutex_unlock(&mutex); 
+
+  printf("Sente-se por favor.\n");
+
+  pthread_mutex_lock(&mutex);
+  total++; // atualiza o total de mensagens
+  pthread_cond_signal(&cond_volte); // sinaliza caso a thread 1 esteja esperando
+  pthread_mutex_unlock(&mutex); 
+
+  return NULL;
+}
+
+void *copoAgua (void *t) {
+  pthread_mutex_lock(&mutex);
+  if (total == 0) { // espera a thread 5
+    pthread_cond_wait(&cond_vinda, &mutex);
+  }
+  pthread_mutex_unlock(&mutex); 
+
+  printf("Aceita um copo d'agua?\n");
+
+  pthread_mutex_lock(&mutex);
+  total++; // atualiza o total de mensagens
+  pthread_cond_signal(&cond_volte); // sinaliza caso a thread 1 esteja esperando
+  pthread_mutex_unlock(&mutex); 
+
+  return NULL;
+}
+
+void *bemVindo (void *t) {
+  printf("Seja bem-vindo! \n");
+
+  pthread_mutex_lock(&mutex);
+  total++; // atualiza o total de mensagens
+  pthread_cond_broadcast(&cond_vinda); // sinaliza caso a thread 1 esteja esperando
+  pthread_mutex_unlock(&mutex);
+
+  return NULL;
+}
+
+// criar as threads
+void createThreads(pthread_t *tids, int numThreads) {
+  int error;
+
+  error = pthread_create(&tids[0], NULL, aVontade, NULL);
+  error = pthread_create(&tids[1], NULL, bemVindo, NULL);
+  error = pthread_create(&tids[2], NULL, senteSe, NULL);
+  error = pthread_create(&tids[3], NULL, copoAgua, NULL);
+  error = pthread_create(&tids[4], NULL, volteSempre, NULL);
+
+  if (error) {
+    fprintf(stderr, "ERRO--pthread_create()\n");
+    exit(1);
+  }
+}
+
+// aguarda o termino das threads
+void joinThreads(pthread_t *tids, int numThreads) {
+  for (int i = 0; i < numThreads; i++) {
+    if (pthread_join(tids[i], NULL)) {
+      fprintf(stderr, "ERRO--pthread_join()\n");
+      exit(2);
+    }
+  }
+}
+
+// inicializa as variaveis de exclusao mutua e condicao
+void init(pthread_mutex_t *mutex, pthread_cond_t *cond_vinda, pthread_cond_t *cond_volte) {
+  pthread_mutex_init(mutex, NULL);
+  pthread_cond_init (cond_vinda, NULL);
+  pthread_cond_init (cond_volte, NULL);
+}
+
+// desaloca as variaveis criadas
+void destroy(pthread_mutex_t *mutex, pthread_cond_t *cond_vinda, pthread_cond_t *cond_volte) {
+  pthread_mutex_destroy(mutex);
+  pthread_cond_destroy(cond_vinda);
+  pthread_cond_destroy(cond_volte);
+}
+
+int main(int argc, char *argv[]) {
+  pthread_t tids[NTHREADS];
+  
+  init(&mutex, &cond_vinda, &cond_volte);
+  createThreads(tids, NTHREADS);
+  joinThreads(tids, NTHREADS);
+  destroy(&mutex, &cond_vinda, &cond_volte);
+
+  return 0;
+}
